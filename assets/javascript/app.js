3c2a31410ccda15d7eee607ca5ddcbd09d774fb6
glaracuente <glaracuente@outbrain.com>
2018-10-20 13:47:12 -0400

    modal final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,9 +14,9 @@ $(document).on("click", '#add-website', function (event) {
     var queryURL = "https://www.googleapis.com/customsearch/v1?q=" + searchTerm + "&cx=004310597913395645264:paiega8jyuo&key=AIzaSyDmxfk0xMoLe6Sup48zTQPXmG0wpcgENyY"
 
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
+        url: queryURL,
+        method: "GET"
+    })
         .then(function (response) {
             console.log(response)
             sites = response.items
@@ -26,8 +26,13 @@ $(document).on("click", '#add-website', function (event) {
                 var auth = '2376-rutgersproject1';
                 var imgUrl = 'http://image.thum.io/get/auth/' + auth + '/' + site;
 
+
+
                 var preview = $("<div>").html('<img class="site" style="-webkit-user-select: none;" src="' + imgUrl + '">').addClass("websites")
                 preview.attr("data-url", site)
+                preview.attr("data-toggle", "modal")
+                preview.attr("data-target", "#myModal")
+
 
                 var info = $("<div>");
                 info.addClass("info");
@@ -49,23 +54,51 @@ $(document).on("click", '#add-website', function (event) {
                 var outerHolder = $("<div>").addClass("outerHolder")
                 holder.append(preview)
                 holder.append(info)
+
+
                 outerHolder.append(holder)
+
                 $("#websitePreview").prepend(outerHolder)
+
 
             }
 
         });
 
-$(document).on("click", '.flip', function () {
-$(".panel").slideToggle("slow");
+    $(document).on("click", '.flip', function () {
+        $(".panel").slideToggle("slow");
 
-});
-$(document).on("click", '.websites', function () {
-    // console.log("test");
-    var url = $(this).attr('data-url');
-    console.log(url)
-    $(".modal-body").html('<iframe width="100%" height="500%" frameborder="0" scrolling="no" allowtransparency="true" src="'+url+'"></iframe>');
-});
+    });
+
+    var modal = document.getElementById('myModal');
+    var span = document.getElementsByClassName("close")[0];
+    var test = $("<div>")
+
+
+    $(document).on("click", '.websites', function () {
+        // Get the modal
+        // Get the <span> element that closes the modal
+        modal.style.display = "block";
+        var url = $(this).attr('data-url');
+        test.html('<iframe width="100%" height="700px" frameborder="0" scrolling="yes" allowtransparency="true" src="' + url + '"></iframe>');
+        $(".modal-content").append(test)
+      // $(".modal-content").html('<iframe width="100%" height="700px" frameborder="0" scrolling="yes" allowtransparency="true" src="' + url + '"></iframe>');
+    });
+
+
+    // When the user clicks on <span> (x), close the modal
+    $(document).on("click", '.close', function () {
+        console.log("tester")
+        modal.style.display = "none";
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    //window.onclick = function (event) {
+    //    if (event.target == modal) {
+    //        modal.style.display = "none";
+    //    }
+   // }
+
 
 
 });
